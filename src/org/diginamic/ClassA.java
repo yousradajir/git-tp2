2a8b38931c7e7507c079addad070db534ff45826
rbonnamy <rbonnamy@diginamic.fr>
2022-09-23 15:33:07 +0200

    Ajout des fichiers ClassA.java et TestB.java

---
+++ src/org/diginamic/ClassA.java
@@ -0,0 +1,12 @@
+package org.diginamic;
+
+public class ClassA {
+	
+	public void method1() {
+		System.out.println("Je suis la méthode 1");
+	}
+	
+	public void method2() {
+		System.out.println("Je suis la méthode 2");
+	}
+}
---
+++ src/org/diginamic/TestB.java
@@ -0,0 +1,12 @@
+package org.diginamic;
+
+public class TestB {
+	
+	public void test1() {
+		System.out.println("Je suis la méthode 1");
+	}
+	
+	public void test2() {
+		System.out.println("Je suis la méthode 2");
+	}
+}
