c87abaef884077982b5f49a54fda21336e3da58a
rbonnamy <rbonnamy@diginamic.fr>
2022-09-23 15:41:03 +0200

    Mise au point de la branche

--- src/org/diginamic/TestB.java
+++ src/org/diginamic/TestB.java
@@ -5,4 +5,8 @@ public class TestB {
 	public void test1() {
 		System.out.println("Je suis la méthode 1");
 	}
+	
+	public void methodeNonInteressante() {
+		System.out.println("Cette méthode ne doit pas être intégrée à la branche master");
+	}
 }
